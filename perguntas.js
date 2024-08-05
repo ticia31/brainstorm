66fef58a177776333bc49b89247ce308db96484f
ticia31 <108100246+ticia31@users.noreply.github.com>
2024-08-05 09:57:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,68 @@
+export const perguntas = [
+    {
+        enunciado: "Assim que saiu do seu estado você se depara com uma cultura diferente e sente dificuldade de se acostumar.Qual o primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "No início ficou com medo do choque cultural.",
+                    "Achou assustador pensar que está em um lugar muito diferente de onde vivia."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                    "Quis saber mais sobre a cultura local.",
+                    "Pensou que ia ser uma experiência incrível."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Quais são os principais aspectos culturais que podem causar choque entre brasileiros de regiões distintas, e como esses choques podem ser  compreendidos de forma mais abrangente?",
+        alternativas: [
+            {
+                texto: "A diversidade de festas e celebrações regionais.",
+                afirmacao: [
+                    "Achou muito diferente as festas e celebrações.",
+                    "Gostou das celebrações.",
+                    "Achou as festas estranhas."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "A diferença de etiquetas e  idioma diferente.",
+                afirmacao: [
+                    "Sentiu dificuldade nos costumes de etiqueta.",
+                    "Sentiu medo e dificuldade no idioma"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "De que maneira as migrações internas no Brasil contribuem para a diversidade cultural do país, mas também podem gerar conflitos e incompreensões entre as diferentes identidades culturais presentes em uma mesma localidade?",
+        alternativas: [
+            {
+                texto: "A mistura de tradições e costumes de diferentes regiões.",
+                afirmacao: [
+                    "Conseguiu utilizar a IA para buscar informações úteis.",
+                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
+                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "O embate entre valores e modos de vida distintos, podem ocasionar os conflitos.",
+                afirmacao: [
+                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
+                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
+                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+];
